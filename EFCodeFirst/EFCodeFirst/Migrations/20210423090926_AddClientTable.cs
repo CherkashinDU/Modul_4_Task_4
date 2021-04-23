7e725904d18a549046e99283b38faffcafe00f15
CherkashinD <cherkashin.du@gmail.com>
2021-04-23 19:46:43 +0300

    add project

---
+++ EFCodeFirst/EFCodeFirst/Migrations/20210423090926_AddClientTable.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCodeFirst.Migrations
+{
+    public partial class AddClientTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employee_Office_OfficeId",
+                table: "Employee");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ClientId",
+                table: "Project",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    ClientId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    StartedDate = table.Column<DateTime>(type: "date", nullable: false),
+                    EndedDate = table.Column<DateTime>(type: "date", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.ClientId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_ClientId",
+                table: "Project",
+                column: "ClientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employee_Office_OfficeId",
+                table: "Employee",
+                column: "OfficeId",
+                principalTable: "Office",
+                principalColumn: "OfficeId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Project_Client_ClientId",
+                table: "Project",
+                column: "ClientId",
+                principalTable: "Client",
+                principalColumn: "ClientId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.Sql(
+                "INSERT INTO " +
+                "Client(Name, Location, StartedDate, EndedDate) " +
+                "VALUES" +
+                "('Microsoft', 'USA', '2019-11-23', NULL), " +
+                "('UkrEnergo', 'Ukraine', '2015-01-10', NULL), " +
+                "('Xiaomi', 'China', '2015-01-10', NULL), " +
+                "('Tuborg', 'Denmark', '2015-01-10', '2019-08-26'), " +
+                "('Alpine', 'Japan', '2020-05-13', NULL)");
+
+            migrationBuilder.Sql(
+                "INSERT INTO " +
+                "Project (Name, Budget, StartedDate, ClientId) " +
+                "VALUES " +
+                "('WebShop', 978783, '2019-11-23', 1), " +
+                "('Provider', 68524, '2015-01-10', 2), " +
+                "('EmailWorker', '356987', '2015-01-10', 3), " +
+                "('BottleDesigner ', '345869', '2015-01-10', 4), " +
+                "('Player', 56489, '2020-05-13', 5)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employee_Office_OfficeId",
+                table: "Employee");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Project_Client_ClientId",
+                table: "Project");
+
+            migrationBuilder.DropTable(
+                name: "Client");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Project_ClientId",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "ClientId",
+                table: "Project");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employee_Office_OfficeId",
+                table: "Employee",
+                column: "OfficeId",
+                principalTable: "Office",
+                principalColumn: "OfficeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ EFCodeFirst/EFCodeFirst/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using EFCodeFirst.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCodeFirst
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
